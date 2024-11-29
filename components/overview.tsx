b235b6804d7db3bfdc436c41b7a3693ca45f950d
Yash Gupta <141564437+guptayash03@users.noreply.github.com>
2024-11-30 04:20:05 +0530

    Update overview.tsx

--- components/overview.tsx
+++ components/overview.tsx
@@ -15,13 +15,13 @@ export const Overview = () => {
     >
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
         <p className="flex flex-row justify-center gap-4 items-center">
-          <VercelIcon size={32} />
-          <span>+</span>
+{/*           <VercelIcon size={32} />
+          <span>+</span> */}
           <MessageIcon size={32} />
         </p>
         <p>
-          This is an{' '}
-          <Link
+          How Can I Help You ?
+{/*           <Link
             className="font-medium underline underline-offset-4"
             href="https://github.com/vercel/ai-chatbot"
             target="_blank"
@@ -33,18 +33,18 @@ export const Overview = () => {
           <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{' '}
           function in the server and the{' '}
           <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
-          on the client to create a seamless chat experience.
+          on the client to create a seamless chat experience. */}
         </p>
         <p>
-          You can learn more about the AI SDK by visiting the{' '}
-          <Link
+          Prompt Your Wish !
+{/*           <Link
             className="font-medium underline underline-offset-4"
             href="https://sdk.vercel.ai/docs"
             target="_blank"
           >
             docs
           </Link>
-          .
+          . */}
         </p>
       </div>
     </motion.div>
