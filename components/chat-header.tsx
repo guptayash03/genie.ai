a287f7197d87a0a31ea3d914afac8039050d8e22
Yash Gupta <141564437+guptayash03@users.noreply.github.com>
2024-11-30 04:08:49 +0530

    Update chat-header.tsx

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -48,7 +48,7 @@ export function ChatHeader({ selectedModelId }: { selectedModelId: string }) {
           target="_noblank"
         >
           <VercelIcon size={16} />
-          Deploy with Vercel
+          Try Premium
         </Link>
       </Button>
     </header>
